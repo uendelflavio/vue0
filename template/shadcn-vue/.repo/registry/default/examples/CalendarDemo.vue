133590b48c2649679a5ec17de5f4e5a33ea68401
Uendel Flávio <uendel.flavio@gmail.com>
2025-06-06 10:02:38 -0300

    several changes, including migration of the radix-vue library to reka-ui, update of the better-sqlite3 library and bug fixes.

---
+++ template/shadcn-vue/.repo/registry/default/examples/CalendarDemo.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import { type DateValue, getLocalTimeZone, today } from '@internationalized/date'
+import { type Ref, ref } from 'vue'
+import { Calendar } from '@/registry/default/ui/calendar'
+
+const value = ref(today(getLocalTimeZone())) as Ref<DateValue>
+</script>
+
+<template>
+  <Calendar v-model="value" :weekday-format="'short'" class="rounded-md border" />
+</template>
